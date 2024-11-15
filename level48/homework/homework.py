fae5b39faaf69b982959ed99f22ae8b5126c80b0
giorgimamisashvili1 <gochamamisashvili93@gmail.com>
2024-11-15 13:43:55 -0800

    commit

---
+++ level48/homework/homework.py
@@ -0,0 +1,14 @@
+# Task 2
+
+# def order_weight(s):
+#     # your code
+    
+#     res = [(sum(int(j) for j in i), i) for i in s.split()]
+    
+#     sorted_res = sorted(res)
+    
+#     return " ".join(i[1] for i in sorted_res)
+
+
+
+# მე აქ შევქმენი ცვალდი res რომელსაც გავუწერე list comarasiion სადაც არის შექმნილი tupler რომელშიც არის რიცხვის თითოეული ელემენტის ჯამი და თვით რიცხვი
